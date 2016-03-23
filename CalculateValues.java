78f594626c206ec1b2b730d3f14a818e6daa9d4b
annuagrawal054 <annuagrawal054@gmail.com>
2016-03-23 19:49:32 +0530

    Added files via upload

---
+++ CalculateValues.java
@@ -0,0 +1,21 @@
+
+public class CalculateValues {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		int y = 10;
+		int a = x + y * 2;
+		int b = x - y +2;
+		int c = (x + y ) *2;
+		int d = y % x;
+		System.out.println("Value of a " +a);
+		System.out.println("Value of b " +b);
+
+		System.out.println("Value of c " +c);
+
+		System.out.println("Value of d " +d);
+
+	}
+
+}
